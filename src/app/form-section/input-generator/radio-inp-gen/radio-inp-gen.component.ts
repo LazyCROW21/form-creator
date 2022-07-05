49a0151346d668bb8077865b8ca7908489ba6d20
LazyCROW21 <sasukeuchiha51047@gmail.com>
2022-07-06 00:24:07 +0530

    all input gen completed

--- src/app/form-section/input-generator/radio-inp-gen/radio-inp-gen.component.ts
+++ src/app/form-section/input-generator/radio-inp-gen/radio-inp-gen.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+type Option = {
+  label: string;
+  value: string;
+};
+
 @Component({
   selector: 'app-radio-inp-gen',
   templateUrl: './radio-inp-gen.component.html',
   styleUrls: ['./radio-inp-gen.component.css']
 })
 export class RadioInpGenComponent implements OnInit {
+  glabel: string = '';
+  options: Option[] = [];
+  optionLabel: string = '';
+  optionValue: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  addOption() {
+    this.options.push({ label: this.optionLabel, value: this.optionValue });
+    this.optionLabel = '';
+    this.optionValue = '';
   }
 
+  removeOption(idx: number) {
+    this.options.splice(idx, 1);
+  }
+
+  isValidOption() {
+    return this.optionLabel && this.optionValue;
+  }
+
+  onAddInput() { }
+
+  isInvalid() { 
+    return this.glabel === '' || this.options.length === 0;
+  }
 }
--- src/app/form-section/input-generator/select-inp-gen/select-inp-gen.component.ts
+++ src/app/form-section/input-generator/select-inp-gen/select-inp-gen.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+type Option = {
+  label: string;
+  value: string;
+};
+
 @Component({
   selector: 'app-select-inp-gen',
   templateUrl: './select-inp-gen.component.html',
   styleUrls: ['./select-inp-gen.component.css']
 })
 export class SelectInpGenComponent implements OnInit {
-
+  label: string = '';
+  options: Option[] = [];
+  optionLabel: string = '';
+  optionValue: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addOption() {
+    this.options.push({ label: this.optionLabel, value: this.optionValue });
+    this.optionLabel = '';
+    this.optionValue = '';
+  }
+
+  removeOption(idx: number) {
+    this.options.splice(idx, 1);
+  }
+
+  isValidOption() {
+    return this.optionLabel && this.optionValue;
+  }
+
+  onAddInput() { }
+
+  isInvalid() {
+    return this.label === '' || this.options.length === 0;
+  }
 }
